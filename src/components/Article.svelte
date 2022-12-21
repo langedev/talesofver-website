9325343efa776b6cb2bb2097137d821a6fd83739
langedev <public@daltonlange.com>
2022-12-20 17:55:49 -0800

    Add highlighting to the title

--- src/components/Article.svelte
+++ src/components/Article.svelte
@@ -4,6 +4,6 @@
 </script>
 
 <article class="px-4 max-w-3xl mx-auto text-stone-400">
-  <h1>{title}</h1>
+  <h1 class="text-2xl sm:text-3xl text-stone-100">{title}</h1>
   <svelte:component this={content} />
 </article>
